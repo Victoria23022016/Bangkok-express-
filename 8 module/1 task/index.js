2e52bd3ae89a76bd249883f7555029c282a17dbb
Виктория <98466732+Victoria23022016@users.noreply.github.com>
2023-06-01 23:49:04 +0700

    Add files via upload

---
+++ 8 module/1 task/index.js
@@ -0,0 +1,81 @@
+import createElement from "../../assets/lib/create-element.js";
+
+export default class CartIcon {
+  constructor() {
+    this.render();
+
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement('<div class="cart-icon"></div>');
+  }
+
+  update(cart) {
+    if (!cart.isEmpty()) {
+      this.elem.classList.add("cart-icon_visible");
+
+      this.elem.innerHTML = `
+        <div class="cart-icon__inner">
+          <span class="cart-icon__count">${cart.getTotalCount()}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
+        </div>`;
+
+      this.updatePosition();
+
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
+    } else {
+      this.elem.classList.remove("cart-icon_visible");
+    }
+  }
+
+  addEventListeners() {
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
+  }
+
+  updatePosition() {
+    if (!this.elem.offsetHeight) {
+      return;
+    }
+
+    if (!this.initialTopCoord) {
+      this.initialTopCoord =
+        this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    let firstIndent =
+      document.querySelector(".container").getBoundingClientRect().right + 20;
+    let secondIndent =
+      document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+    let leftIndent = Math.min(firstIndent, secondIndent) + "px";
+
+    if (window.pageYOffset > this.initialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        top: "50px",
+        zIndex: 1e3,
+        right: "10px",
+        left: leftIndent,
+      });
+    }
+
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+    }
+  }
+}
